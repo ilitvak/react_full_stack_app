36f1bb3fb66f81b7c35baaa8e33cc8ed9c1df53a
Irwin <irwin.litvak@gmail.com>
2018-07-10 16:28:34 -0400

    pushing latest work

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const HTMLWebpackPlugin = require('webpack-plugin');
+let HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + './app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
   // have webpack transform all JSX code found in this entry point to ES5
-  entry: __dirname + './app.js',
+  entry: __dirname + '/app/app.js',
   // once webpack grabs the above files, you can tell it what to do in the below module prop.
   // Each "loader" that you add to the loaders array will represent a transformation that your 
   // code will go through before reaching the browser.
@@ -9,8 +16,16 @@ module.exports = {
     // that your code will go through before it reaches the browser
     loaders: [
       {
-        test: /\.js$/
+        // the loader will perform all transformations on files that end in .js
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
     ]
-  }
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebpackPluginConfig]
 };
